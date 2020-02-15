3cca9a7c435ab1a9f2ff7e80b0a7bf01cd069d13
patcantin <patcantin23@gmail.com>
2020-02-15 08:28:20 -0500

    half lecture, flat err

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,11 +1,14 @@
+# app/controllers/flats_controller.rb
 class FlatsController < ApplicationController
   def index
-    @flats = Flat.where.not(latitude: nil, longitude: nil)
+    @flats = Flat.geocoded
 
     @markers = @flats.map do |flat|
       {
         lat: flat.latitude,
-        lng: flat.longitude
+        lng: flat.longitude,
+        infoWindow: render_to_string(partial: "infowindow", locals: { flat: flat }),
+        # image_url: helpers.asset_url('REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS')
       }
     end
   end
--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -3,13 +3,20 @@ import mapboxgl from 'mapbox-gl';
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
+  const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+};
+
   if (mapElement) { // only build a map if there's a div#map to inject into
-    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    mapboxgl.accessToken = process.env.MAPBOX_API_KEY;
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
   }
+   fitMapToMarkers(map, markers);
 };
 
 export { initMapbox };
